1f28f74dcd1cb8143d6a1824c33e21558e2feac3
Ronnie Song <ronniesong0809@gmail.com>
2020-06-22 22:11:10 -0700

    add tower

--- tutorial/section 5/main.js
+++ tutorial/section 5/main.js
@@ -1,7 +1,17 @@
-var roleHarvester = require("role.harvester")
+var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+
+    var tower = Game.getObjectById('f499c508e7e666464ae390aa');
+    if(tower) {
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
@@ -9,6 +19,9 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }
